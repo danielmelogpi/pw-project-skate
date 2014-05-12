0192b336a32a1f999c4d694d0963a5585f0e46b5
Daniel Melo <danielmelogpi@gmail.com>
2014-05-11 21:26:25 -0300

    zoom on marker

--- js/index.js
+++ js/index.js
@@ -12,6 +12,8 @@ function init() {
 	map = new google.maps.Map(document.getElementById("map"),
 		mapOptions);
 
+	map.markers = [];
+
 }
 
 /** adiciona os marcadores ao mapa */
@@ -32,8 +34,30 @@ function plotPoint(map, point) {
 		title:"Local",
 		icon: "img/icon/bart-skate-map.png"
 	});
+
+	/* adiciona evento */
+	google.maps.event.addListener(marker, 'click', function(event) {
+			zoomOnMarker(event, marker);
+			addButtons();
+		}
+	);
+
+	/** adiciona ao array do mapa, para controle */
+	map.markers.push(marker);
+
 }
 
+/* centraliza e da zoom em um marcador */
+function zoomOnMarker (event, marker) {
+	var center =  new google.maps.LatLng(event.latLng.lat(), event.latLng.lng());
+	map.setCenter(center);
+	map.setZoom(16);
+}
+
+function addButtons() {
+	var nav = $("<button value='navegar até aqui>'");
+	$(".buttons");
+}
 
 /** Retorna os pontos com os picos */
 function getPoints() {
